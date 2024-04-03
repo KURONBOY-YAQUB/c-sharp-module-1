28ad52ece5bf899b0d4b94e8f0c090677eae5006
Yakubov01 <lord17cl@gmail.com>
2024-04-03 06:28:37 +0500

    ready empty metods

--- CSharpModule.Tasks/StringOperations.cs
+++ CSharpModule.Tasks/StringOperations.cs
@@ -2,5 +2,39 @@ namespace CSharpModule.Tasks;
 
 public class StringOperations
 {
-    
+    public string Reverse(string str) 
+    {
+        return str;
+    }
+
+    public bool AreAnagrams(string str1, string str2)
+    {
+        return false;
+    }
+
+    public string LongestPalindromeWithin(string str) 
+    {
+        return str;
+    }
+
+    public string RemoveDuplicates(string str) 
+    {
+        return str;
+    }
+
+    public bool IsValidPalindrome(string str) 
+    {
+        return false;
+    }
+
+    public char FirstNonRepeatedCharacter(string str) 
+    {
+        return 'a';
+    }
+
+    public string Compress(string str) 
+    {
+        return str;
+    }
+
 }
